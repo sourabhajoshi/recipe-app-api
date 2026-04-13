dff195886a545347c527cbe6d6469de054de4130
Joshi Sourabha <sourabhjoshi124@gmail.com>
2026-04-13 12:17:44 +0530

    DB waiting command and test case

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""
+Django command to wait for the database to be avilable
+"""
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+  """Django command wait for database"""
+  
+  def handle(self, *args, **options):
+    """Entrypoint for command"""
+    self.stdout.write("Waiting for database...")
+    db_up = False
+    while db_up is False:
+      try:
+        self.check(databases=["default"])
+        db_up = True
+      except(Psycopg2Error, OperationalError):
+        self.stdout.write("database unavilable, waiting for 1 secon...")
+        time.sleep(1)
+        
+    self.stdout.write(self.style.SUCCESS('Database avilable!'))
+      
