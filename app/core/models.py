20adb0d690b818d2dbd281345f65535eac26d690
Joshi Sourabha <sourabhjoshi124@gmail.com>
2026-04-13 18:02:51 +0530

    TDD user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+"""
+database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+class Usermanager(BaseUserManager):
+  """manager for users"""
+  def create_user(self, email, password=None, **extra_fields):
+    """create, save and return the new user"""
+    user = self.model(email=email, **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+  email= models.EmailField(max_length=255, unique=True)
+  name= models.CharField(max_length=255)
+  is_active= models.BooleanField(default=True)
+  is_staff= models.BooleanField(default=False)
+  
+  objects = Usermanager()
+  
+  # field we use for authentication
+  USERNAME_FIELD = 'email'
+   
